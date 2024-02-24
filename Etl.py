ab12c626c3e6caa8c742e37a319b298d70af4c0a
Arda Bakır <abakir@carex.ai>
2024-02-24 05:02:17 +0300

    Add comments and do refactoring

--- Etl.py
+++ Etl.py
@@ -9,13 +9,13 @@ class Etl:
     def __init__(self, file_path):
         self.file_path = file_path
 
-
+    # Extract data from the file
     def extract(self):
         with open(self.file_path, 'r', encoding="utf8") as file:
             data = file.read()
         return data
 
-
+    # Transform the data into a format that can be loaded into the database
     def transform(self, data):
 
         prod_dict = xmltodict.parse(data, process_namespaces=True)
@@ -24,12 +24,12 @@ class Etl:
 
         return products
 
-
+    # Load the data into the database
     def load(self, data):
         db = Database()
         db.upsert_many(map(lambda x: x.__dict__, data))
 
-
+    # Arrange the product data into a list of Product objects
     def __arrange_product_data(self, products):
         products_data = []
         for product in products:
@@ -56,10 +56,15 @@ class Etl:
         soup = BeautifulSoup(description, 'html.parser')
         list_items = soup.find_all('li')
         desc_dict = {}
+        #find strong tags in the list items 
+        #strong tags are used to identify the description items
         for item in list_items:
             bold = item.find('strong')
             if bold == None:
                 continue
+            #find the index of the colon in the bold text
+            #if the bold text contains a colon, it is a description item
+            #if it doesn't contain a colon, it is the sample size
             colon_index = bold.text.find(':')
             if colon_index == -1:
                 desc_dict["sample_size"] = bold.text.strip()
@@ -67,7 +72,7 @@ class Etl:
             desc_dict = self.__process_description(item, bold, colon_index, desc_dict)
         return desc_dict
     
-
+    # process the description items to better fit the product data and translate the description keys
     def __process_description(self, item, bold, ind, dict):
         if bold.text[:ind].strip().lower() == "kumaş bilgisi":
                 dict["fabric"] = item.text[ind+1:].strip()
@@ -78,25 +83,22 @@ class Etl:
 
         return dict
 
+    # process the product details and change the key names to better fit the product data
     def __process_product_detail(self, productDetail):
         key = productDetail["@Name"].lower()
         if key == "price":
             key = productDetail["@Name"].lower()
             val = float(productDetail["@Value"].replace(',','.'))
-
         elif key == "discountedprice":
             key = "discounted_price"
             val = float(productDetail["@Value"].replace(',','.'))
-        elif key == "quantity":
-            val = int(productDetail["@Value"])
         elif key == "color":
             val = productDetail["@Value"].split(',')
-        elif key == "series":
-            val = productDetail["@Value"]
-        elif key == "season":
-            val = productDetail["@Value"]
         elif key == "producttype":
             key = "product_type"
+            val = productDetail["@Value"]
+        elif key == "priceunit":
+            key = "price_unit"
             val = productDetail["@Value"]
         else:
             val = productDetail["@Value"]
